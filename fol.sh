1f4ebf182eeff222c81028d2c9224f1921961d1a
aravind <aravindkukku>
2018-07-23 15:37:56 +0530

    kuk commited

--- fol.sh
+++ fol.sh
@@ -1,1 +1,12 @@
-read -p "enter the name of file"
+read -p "enter the name of file" ex
+mkdir /home/aravind/$ex;
+for i in range $(seq 3)
+ do
+ nano /home/aravind/$ex/file$i
+done
+tar -cf /home/aravind/$ex.tar /home/aravind/$ex
+git pull origin master
+git add -A
+git commit -m "$ex commited"
+git push origin master
+
